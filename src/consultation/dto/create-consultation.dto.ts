3f4a32906c37eb62c9e2e1da06af4d2bf0100a25
SaitgalinAygiz <saitgalin18@gmail.com>
2020-05-28 14:31:22 +0500

    consultations create fix

--- src/consultation/dto/create-consultation.dto.ts
+++ src/consultation/dto/create-consultation.dto.ts
@@ -1,5 +1,5 @@
 import {ApiProperty} from "@nestjs/swagger";
-import {IsArray, IsDate, IsNotEmpty, IsNumber, IsString} from "class-validator";
+import {IsArray, IsDate, IsDateString, IsNotEmpty, IsNumber, IsString} from "class-validator";
 
 export class CreateConsultationDto {
 
@@ -26,6 +26,6 @@ export class CreateConsultationDto {
     teacher: string
 
     @ApiProperty()
-    @IsNumber()
-    time: number
+    @IsDateString()
+    time: Date
 }
